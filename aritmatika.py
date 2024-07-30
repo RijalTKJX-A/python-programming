ec959f070da2aed7ee8a06495c4b5893e954f1ef
RijalTKJX-A <rijalraden808@gmail.com>
2024-07-30 09:40:24 +0700

    Add files via upload

---
+++ aritmatika.py
@@ -0,0 +1,16 @@
+bil1 = 9
+bil2 = 7
+
+print("penjumlahan :", bil1 + bil2)
+
+print("pengurangan :", bil1 - bil2)
+
+print("perkalian :",bil1 * bil2)
+
+print("pembagian :",bil1 / bil2)
+
+print("perpangkatan :",bil1 ** bil2)
+
+print("modulus :",bil1 % bil2)
+
+print("pembagian bulat :",bil1 // bil2) 
