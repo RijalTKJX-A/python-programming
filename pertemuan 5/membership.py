8fdc85502056c32652276e93fe083f33b6cdfe0e
RijalTKJX-A <rijalraden808@gmail.com>
2024-08-06 09:42:58 +0700

    Add files via upload

---
+++ pertemuan 5/membership.py
@@ -0,0 +1,7 @@
+makanan = ["kue pancong", "nasi goreang", "burger", "lotek"]
+
+print("Apakah nasi uduk merupakan anggota", "nasi uduk" in makanan)
+print("apakah burger merupakan anggota?", "burger" in makanan)
+
+print("apakah nasi uduk merupakan anggota?", "nasi uduk" not in makanan)
+print("apakah burger merupakan anggota?", "burger" not in makanan)
---
+++ pertemuan 5/nilai.py
@@ -0,0 +1,4 @@
+nilai = 80
+
+if nilai > 90 and nilai <= 100:
+|  print("Grade A")
