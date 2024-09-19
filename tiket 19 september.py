41da212b3a61288d05bce2ddef31067c270e3664
RijalTKJX-A <rijalraden808@gmail.com>
2024-09-19 12:04:27 +0700

    Add files via upload

---
+++ tiket 19 september.py
@@ -0,0 +1,24 @@
+def hitung_harga(usia,jumlah_tiket):
+    if usia < 12:
+        harga = 30.000 # harga
+        tiket anak_anak
+    elif usia <= 60:
+        harga = 50.000 # harga
+        tiket dewasa
+    else:
+        harga = 35.000 # harga
+        tiket lansia
+        
+        total_harga = harga * jumlah_tiket
+        return total_harga
+    
+        #memproses beberapa pembeli tiket
+        jumlah_pembeli
+        int(input("Masukjan jumlah_pembeli:"))
+        
+        for i in range(jumlah_pembeli;)
+            print(f"\nPwmbeli { i + i}:")
+            usia = int(input("Masukan usia pembeli: "))
+            # hitung total harga tiket
+            total = hitung_harga(usia,jumlah_tiket)
+            print(f"Total harga untuk pembeli { 1 + 1 }: Rp: (total")
